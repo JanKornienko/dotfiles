f9daf7c3fe916f4559ab38e4cdc2b68cc3c45d6c
Jan Kornienko <kor.jan@outlook.com>
2021-04-13 20:18:18 +0200

    Emacs

---
+++ .emacs.d/init.el
@@ -0,0 +1,5 @@
+(setq inhibit-startup-message t) ;Disable welcome tips
+(scroll-bar-mode -1) ;Disable visible scrollbar
+(tool-bar-mode -1) ;Disable the toolbar
+(tooltip-mode -1) ;Disable tooltips
+(menu-bar-mode -1) ;Disable the menu bar
