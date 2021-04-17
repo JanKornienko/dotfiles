f9e36c3f8545d179783bdf303c133fd2e7d4edcb
Jan Kornienko <kor.jan@outlook.com>
2021-04-17 22:05:14 +0200

    EVIL file customization

--- .emacs.d/elisp/base.el
+++ .emacs.d/elisp/base.el
@@ -3,17 +3,14 @@
 (tool-bar-mode -1) ; Disable toolbar
 (menu-bar-mode -1) ; Disable menu bar
 (setq-default tab-width 2) ; Tab widht 2
-(global-display-line-numbers-mode) ; Line numbers
 (fringe-mode 0)
 
 ;; Package repositories
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/") t)
+(package-refresh-contents)
 (package-initialize)
-
-(unless package-archive-contents
-  (package-refresh-contents))
 
 ;; Install use-package
 (unless (package-installed-p 'use-package)
--- .emacs.d/elisp/vim.el
+++ .emacs.d/elisp/vim.el
@@ -1,9 +1,16 @@
-;; Download Evil
-(unless (package-installed-p 'evil)
-  (package-install 'evil))
-
-;; Enable Evil
-(require 'evil)
-(evil-mode 1)
+;; Evil Mode
+(use-package evil
+             :ensure t ; install evil if not installed
+             :init ; tweak evil's configuration before loading it
+             (setq evil-want-integration t)
+             (setq evil-want-keybinding nil)
+             (setq evil-vsplit-window-right t)
+             (setq evil-split-window-below t)
+             (evil-mode))
+(use-package evil-collection
+             :after evil
+             :ensure t
+             :config
+             (evil-collection-init))
 
 (provide 'vim)
