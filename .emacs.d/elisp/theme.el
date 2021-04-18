575a0b634565df2dc29d77f338d111f51acc14e0
Jan Kornienko <kor.jan@outlook.com>
2021-04-18 14:50:26 +0200

    Emacs ORG config

--- .emacs.d/elisp/theme.el
+++
@@ -1,6 +0,0 @@
-(use-package gruvbox-theme
-  :defer t
-  :init
-  (load-theme 'gruvbox-dark-hard t))
-
-(provide 'theme)
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,11 +1,4 @@
-;; Loading configuration files from 'elisp' folder 
-(add-to-list 'load-path (concat user-emacs-directory "elisp"))
-
-;; Base configuration
-(require 'base)
-
-;; Theme
-(require 'theme)
-
-;; Evil mode
-(require 'vim)
+(org-babel-load-file
+  (expand-file-name
+    "config.org"
+    user-emacs-directory))
